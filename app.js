fc779d5f9bed2770a596a8a6bca2999f86015e1c
Satyaki Bose <satyakibose98@gmail.com>
2020-11-28 12:48:24 +0530

    input-processing-ouput

--- app.js
+++ app.js
@@ -2,9 +2,29 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+  console.log("Error occurred", error);
+  alert("Something wrong with the server! Please try again after some time.");
+}
 
 function clickHandler() {
-  outputDiv.innerText = "svhvafa" + txtInput.value;
+  var inputText = txtInput.value; //taking input
+
+  // Calling server for processing
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText; // Showing output
+    })
+    .catch(errorHandler);
 }
 
 btnTranslate.addEventListener("click", clickHandler);
